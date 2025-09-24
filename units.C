95bdc4554d168bac01e547e3c59ab5db855bb5d1
cozentrevous <minatrevy@gmail.com>
2025-09-24 11:29:55 +0300

    Add files via upload

---
+++ units.C
@@ -0,0 +1,20 @@
+//simple program to display all units for this semester
+/*
+Name:Cozentrevous Agosa
+Reg no:PA106/G/28712/25
+Description:program to display all units for this semester,software engineering, first year
+*/
+#include<stdio.h> //preprpcessor directive :scanf(),printf()
+
+// main function
+int main()  {
+    printf("SPM 2100 discrete mathematics \n" );
+	printf("SPM 2104 mathematics for science \n");
+	printf("UCU 2105 fundamentals of ICT \n");
+	printf("SPI 2102 computer systems and organization \n");
+    printf("UCU 2101 communication skills \n");
+	printf("SPC 2101 introduction to programming and problem solving \n");
+	printf("SSE 2101 fundamentals of system analysis and design \n");
+	                                                                      
+	return 0;
+}
